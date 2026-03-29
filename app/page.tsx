b6a47a04638d4a6e20f909163a5e4c1c42176b09
Adesh Katiya <adeshkatiya27@gmail.com>
2026-03-30 00:06:13 +0530

    feat(app): update Yjs editor state sync behavior with indexeddb

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 import * as Y from "yjs";
 import { useState, useEffect } from "react";
+import { IndexeddbPersistence } from "y-indexeddb";
 
 const ydoc = new Y.Doc();
 const yText = ydoc.getText("editor");
 
 export default function Home() {
   const [text, setText] = useState("");
+  const persistence = new IndexeddbPersistence("my-doc", ydoc);
 
   useEffect(() => {
     // Initial load
     setText(yText.toString());
 
     // Listen for changes
+    yText.observe(() => {
+      setText(yText.toString());
+    });
+  }, []);
+
+  useEffect(() => {
+    persistence.whenSynced.then(() => {
+      setText(yText.toString());
+    });
+
     yText.observe(() => {
       setText(yText.toString());
     });
